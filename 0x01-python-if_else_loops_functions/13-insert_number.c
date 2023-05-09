eb3c9caed0c41c75f79121eb5070d45441fd98bb
mathewos <mathewos1@yahoo.com>
2023-05-09 15:58:21 -0700

    c singly linked node addd

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,28 @@
+#include "lists"
+/**
+ * insert_node - inserts number at a node in assending order
+ * @head: list to be altered
+ * @number: number to be added
+ * Return: pointer to node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node, *insert;
+
+	insert = malloc(sizeof(listint_t));
+	if (insert == NULL)
+		return (NULL);
+	insert->n = number;
+	node = *head;
+	if (node == NULL && node->n >= number)
+	{
+		insert->next = next;
+		*head = insert;
+		return (insert);
+	}
+	while (node && node->next && node->next < number)
+		node = node->next;
+	insert->next = node->next;
+	node->next = insert;
+	return (insert);
+}
