739ff7dcffc7bb38c7c312202e2d614efc3762cc
mathewos <mathewos1@yahoo.com>
2023-05-09 15:22:25 -0700

    python if and for loop Q 5 to 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 101):
+        fizz = num % 3
+        buzz = num % 5
+        if fizz == 0 and buzz == 0:
+            print("FizzBuzz ", end="")
+        elif fizz == 0:
+            print("Fizz ", end="")
+        elif buzz == 0:
+            print("Buzz ", end="")
+        else:
+            print(f"{num} ", end="")
