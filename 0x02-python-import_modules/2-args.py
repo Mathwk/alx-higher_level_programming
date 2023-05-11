817015fdeeed9439887806fd7fbffe36764a722c
mathewos <mathewos1@yahoo.com>
2023-05-11 13:39:04 -0700

    python import module Q2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argc = len(sys.argv) -1
+    argc = len(sys.argv) - 1
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
