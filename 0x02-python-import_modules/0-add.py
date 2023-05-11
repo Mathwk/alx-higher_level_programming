1c90c5c353fe31367276d2b2f15468b4c534ace7
mathewos <mathewos1@yahoo.com>
2023-05-11 12:26:46 -0700

    python module Q 0&1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin//python3
-a = 1
-b = 2
-from add_0 import add
-c = add(a, b)
-print(f"{a} + {b} = {c}")
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    from add_0 import add
+    c = add(a, b)
+    print("{} + {} = {}".format(a ,b c))
