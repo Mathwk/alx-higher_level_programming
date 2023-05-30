5923c2da81463dd4d57873391256ffc7896fb7fc
mathewos <mathewos1@yahoo.com>
2023-05-29 17:11:20 -0700

    python exeption

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        execute = fct(*args)
+        return (execute)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
