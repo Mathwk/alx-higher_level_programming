f1d7bd22a146093f2a5ba8d9ed61599bc69c5d37
Mathewos K <mathewos1@yahoo.com>
2023-07-19 15:08:12 -0700

    sql queries Q 10 - 16

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+-- Import the database dump from hbtn_0d_tvshows
+SELECT tv_show_geners LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title ASC;
