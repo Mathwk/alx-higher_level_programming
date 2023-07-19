4c2f967ddbb53edfb89acdc2ec218519829b856a
Mathewos K <mathewos1@yahoo.com>
2023-07-19 15:28:58 -0700

    sql queries Q 10-16 correction

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- Import the database dump from hbtn_0d_tvshows
-SELECT tv_genres LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_genres.name ASC;
+SELECT tv_genres.namefrom tv_show_genres LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name ASC;
